cb341c3bf5f5c810628080040727f892787e93fd
Alex O <mightycron@gmail.com>
2025-07-19 18:01:57 +0500

    Scripts

--- scripts/dup.mjs
+++ scripts/dup.mjs
@@ -1,10 +1,12 @@
 import assert from 'node:assert'
+import { log } from 'node:console'
 import test from 'node:test'
 
 function parseLeafs(txt) {
+    const tabSpaces = 4
     const states = {
-        SEARCH,
-        ENTRY,
+        SEARCH: 'search',
+        ENTRY: 'entry',
     }
 
     let curState = states.SEARCH
@@ -12,6 +14,9 @@ function parseLeafs(txt) {
     let prevSpacesCount = 0
     let spacesCount = 0
     let curPayload = []
+    let siblings = []
+    let leafs = []
+    states_loop:
     while (true) {
         const cur = pos >= txt.length ? null : txt[pos]
 
@@ -19,13 +24,13 @@ function parseLeafs(txt) {
             case states.SEARCH:
                 switch (cur) {
                     case null:
-                        return
+                       break states_loop 
                     case '\n':
                         spacesCount = 0
                         pos++
                         break
                     case '\t':
-                        spacesCount += 4
+                        spacesCount += tabSpaces
                         pos++
                         break
                     case ' ':
@@ -41,19 +46,104 @@ function parseLeafs(txt) {
                 switch (cur) {
                     case null:
                     case '\n':
+                        const spacesDiff = Math.round(
+                            (spacesCount - prevSpacesCount) / tabSpaces)
+
+                        const payload = curPayload.join('')
+
+                        if (spacesDiff == 0) {
+                            siblings.push({
+                                payload,
+                                parent: siblings.length > 0 ? siblings[0].parent : null,
+                            })
+                        } else if (spacesDiff > 0) {
+                            siblings = [{
+                                payload,
+                                parent: siblings[siblings.length - 1] || null,
+                            }]
+                        } else if (spacesDiff < 0) {
+                            siblings.forEach(node => leafs.push(node))
+
+                            let parent = siblings[siblings.length - 1].parent
+                            for (let i = 0; i < Math.abs(spacesDiff); i++) {
+                                parent = parent.parent
+                            }
+
+                            siblings = [{
+                                payload,
+                                parent,
+                            }]
+                        }
+
+                        curPayload = []
                         prevSpacesCount = spacesCount
+                        curState = states.SEARCH
                         break
                     default:
                         curPayload.push(cur)
                         pos++
                         break
                 }
+                break
             default:
-                throw new Error('unknown state')
+                throw new Error('unknown state "' + curState + '"')
         }
     }
+
+    siblings.forEach(node => leafs.push(node))
+
+    return leafs
 }
 
 test('empty', () => {
-    assert.strictEqual(1, 1)
+    assert.deepEqual(parseLeafs(""), [])
+})
+
+
+test('files at root', () => {
+    assert.deepEqual(
+        parseLeafs(`
+            f1
+            f2
+            f3
+        `),
+        [
+            {payload: 'f1', parent: null},
+            {payload: 'f2', parent: null},
+            {payload: 'f3', parent: null},
+        ],
+    )
+})
+
+test('dir with files', () => {
+    assert.deepEqual(
+        parseLeafs(`
+            d1
+                f1
+                f2
+        `),
+        [
+            {payload: 'f1', parent: {payload: 'd1', parent: null},},
+            {payload: 'f2', parent: {payload: 'd1', parent: null},},
+        ],
+    )
+})
+
+test('two dirs with files', () => {
+    assert.deepEqual(
+        parseLeafs(`
+            d1
+                f1
+                f2
+            d2
+                f3
+                f4
+        `),
+        [
+            {payload: 'f1', parent: {payload: 'd1', parent: null},},
+            {payload: 'f2', parent: {payload: 'd1', parent: null},},
+            {payload: 'f3', parent: {payload: 'd2', parent: null},},
+            {payload: 'f4', parent: {payload: 'd2', parent: null},},
+        ],
+    )
 })
